ca09a947f48706845c7336d89341dd116c78a0c9
Heesung jang <heesungj7@gmail.com>
2021-06-18 12:09:46 +0900

    update

--- ArrayForm.js
+++ ArrayForm.js
@@ -4,28 +4,34 @@ const onClickButton = () => {
 const buttons1 = document.getElementById("btn1");
 const buttons = document.getElementsByClassName("btn");
 
+// console.log(buttons);
+// buttons.forEach((button) => {
+//     button.addEventListener("click", onClickButton);
+// });
+
 Array.from(buttons).forEach((element) => {
     element.addEventListener("click", onClickButton);
 });
 
-buttons1.addEventListener("click", onClickButton);
+// buttons1.addEventListener("click", onClickButton);
 
 const tempList = [1, 2, 3, 4, 5];
 
-tempList.forEach((num) => {
-    console.log(num);
-});
+// tempList.forEach((num) => {
+//     console.log(num);
+// });
 
-const friends = ["nico", "hee", "zion", "minsoo"];
+// const friends = ["nico", "hee", "zion", "minsoo"];
 
-for (let i = 0; i < friends.length; i++) {
-    console.log(`heelo ${friends[i]} times`);
-}
+// for (let i = 0; i < friends.length; i++) {
+//     console.log(`heelo ${friends[i]} times`);
+// }
 
-const addHeart = (c, i, a) => console.log(c, i, a);
+const friends = [1, 2, 3, 4, 5];
+const addHeart = (c, i) => console.log(c, i);
 
 friends.forEach(addHeart);
 
-for (const friend of friends) {
-    console.log(friend);
-}
+// for (const friend of friends) {
+//     console.log(friend);
+// }
